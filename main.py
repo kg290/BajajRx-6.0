ed021bb215216f52278a168a9872ebbcfee0f311
Karnajeet Gosavi <karnajeet.gosavi23@vit.edu>
2025-07-29 00:07:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -467,4 +467,4 @@ if __name__ == "__main__":
     import uvicorn
 
     print(f"[kg290] Starting server with Scout Model: {SCOUT_MODEL}")
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=port)
